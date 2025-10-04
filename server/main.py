f38074f0ce63ba3ea6aeef0df4b110cafc8dacb9
tiggerbob <tiggerbob50@gmail.com>
2025-10-04 15:01:58 -0500

    Add backend GET endpoint

--- server/main.py
+++ server/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+from database import db
 
 app = FastAPI()
 
@@ -12,3 +16,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/kudos/", status_code=200)
+def read_items():
+    json_db = []
+    for item in db:
+        json_db.append(jsonable_encoder(item))
+
+    return JSONResponse(json_db)
